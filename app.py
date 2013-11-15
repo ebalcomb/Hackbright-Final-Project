545a561390789e67ace00fb3e80f4d0f58a46110
Elli <Elli@Ellis-MacBook-Air.local>
2013-11-15 14:37:08 -0800

    added address autofill

--- app.py
+++ app.py
@@ -9,55 +9,33 @@ app.secret_key = "Dumbledore"
 
 @app.route("/")
 def index():
-    user_id = session.get("user_id")
-    if user_id:
-    	pass
-        #return redirect("/users/%s" %user_id)
-    else:
-    	pass
-        #return render_template("index.html")
-    return render_template("test.html")
+    return render_template("landing.html")
 
-@app.route("/", methods=["POST"])
-def login():
-    email = request.form.get("email")
-    password = request.form.get("password")
+@app.route("/route")
+def route():
+    return render_template("routing.html")
 
-    user_info = model.authenticate(email, password)
-    if user_info != None:
-    	pass
-        #session['user_id'] = user_info[0]
-        #session['email'] = user_info[1]
-        #return redirect("/users/%s" %user_info[0])
-    else:
-    	pass
-        #flash("Your login credentials are not recognized. Sorry!")
-    return render_template("master.html")
-
-@app.route("/register", methods=["POST"])
-def register():
-	pass
-    #r_email = request.form.get("r_email")
-    #r_password = request.form.get("r_password")
-    #password_verify = request.form.get("password_verify")
-    #age = request.form.get("age")
-    #zip_code = request.form.get("zip_code")
+@app.route("/route", methods=["POST"])
+def process_route():
+    start = request.form.get("start")
+    end = request.form.get("end")
 
 
-    # user_email = model.register_check(r_email)
-    # if user_email == True:
-    #     flash("This email is already in use. Please try a new email or login to an existing account.")
-    #     return redirect(url_for ("index"))  
-    # else:
-    #     if r_password == password_verify:
-    #         user_id = model.register_store(r_email, r_password, age, zip_code)
-    #         return redirect("/users/%s" %user_id)
-    #     else:
-    #         flash("Your passwords did not match, please try again.")
-    #         return redirect(url_for("index"))
+    return render_template("landing.html")
 
 
-#log out of session
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
+@app.route("/getinvolved")
+def involved():
+    return render_template("involved.html")
+
 @app.route("/clear")
 def clear_session():
     session.clear()
